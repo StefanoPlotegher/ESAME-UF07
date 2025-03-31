d900ab4129595acbded73e12f06b537ddb5d67cf
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2025-03-31 10:46:50 +0200

    feat: implementazione iniziale del server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+/**
+ * Avvio del server
+ * @param {number} PORT - Porta sulla quale il server deve ascoltare
+ * @param {function} callback - Funzione di callback che conferma l'avvio del server
+ */
+app.listen(PORT, () => {
+  console.log(`Server in ascolto sulla porta: ${PORT}`);
+});
