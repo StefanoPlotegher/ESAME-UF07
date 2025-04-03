7a9736315103d23df32259a86ee14f640282ccde
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2025-04-03 11:55:07 +0200

    feat: nuovo file js per evitare problemi con le variabili

---
+++ TaskManager/public/src/task.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const taskId = urlParams.get('taskId');
+    const form = document.getElementById('form');
+
+});
