e202520b769db0af6f8c20006336f4c18c5a9d61
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2025-04-03 12:06:25 +0200

    feat: aggiornato il file visualizzaTask.html e task.js per supportare la modifica delle task

--- TaskManager/public/src/task.js
+++ TaskManager/public/src/task.js
@@ -6,13 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //recupero i dati delle task
     let tasks = [];
-    async function loadTasks() {
+    async function caricaTask() {
         try {
             const response = await fetch("/tasks");
             if (!response.ok) {
                 throw new Error(`Errore nel caricamento delle task: ${response.statusText}`);
             }
             tasks = await response.json();
+            visualizzaTask();
         } catch (error) {
             console.error(error.message);
         }
@@ -36,5 +37,38 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('stato').value = task.stato;
     }
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
 
+        const taskMod = {
+            titolo: document.getElementById('titolo').value.trim(),
+            data: document.getElementById('data').value,
+            desc: document.getElementById('desc').value.trim(),
+            priorita: document.getElementById('priorita').value,
+            stato: document.getElementById('stato').value,
+        };
+
+        tasks[id] = taskMod;
+
+        salvaTask();
+        window.location.href = "../index.html";
+    });
+
+    /**
+     * funzione per salvare la lista modificata di task sul server
+     */
+
+    async function salvaTask() {
+        try {
+            await fetch('/mod', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(tasks)
+            });
+        } catch (err) {
+            console.error("Errore nel salvataggio della task", err.message);
+        }
+    }
+
+    caricaTask();
 });
