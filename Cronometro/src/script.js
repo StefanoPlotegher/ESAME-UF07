4595d623cc69891f1a6fa5f9dce5ce238de55406
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2025-04-13 16:03:07 +0200

    feat: create variabili e costanti nel file JS

---
+++ Cronometro/src/script.js
@@ -0,0 +1,9 @@
+let inizio = 0;
+let tTrascorso = 0;
+let inCorso;
+
+const tempo = document.getElementById("tempo");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
