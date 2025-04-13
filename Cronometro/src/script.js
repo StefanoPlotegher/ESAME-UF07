d37c90f67f895c3afe9527685652f5004cd9def1
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2025-04-13 16:13:01 +0200

    feat: funzioni per l'avvio e aggiornamento del cronometro

--- Cronometro/src/script.js
+++ Cronometro/src/script.js
@@ -1,9 +1,33 @@
-let inizio = 0;
+let tInizio = 0;
 let tTrascorso = 0;
-let inCorso;
+let intervallo;
 
 const tempo = document.getElementById("tempo");
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
 
+/**
+ * Funzione per aggiornare il cronometro
+ */
+function aggiorna(){
+    const now = Date.now();
+    tTrascorso = now - tInizio;
+    tempo.textContent = tTrascorso;
+}
+
+
+/**
+ * Event listener per vviare il cronometro
+ * e aggiornarlo ogni 10 millisecondi
+ * disabilta il pulsante start
+ */
+start.addEventListener("click", () =>{
+    if (!intervallo){
+        tInizio = Date.now() - tTrascorso;
+        intervallo = setInterval(aggiorna, 10);
+        start.disabled = true;
+        reset.disabled = true;
+        stop.disabled = false;
+    }
+});
